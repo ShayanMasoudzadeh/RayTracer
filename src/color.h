fa9ce6aa32c923f7c8956f7c1a4a15777b3360e5
ShayanMasoudzadeh <shayan.masoudzadeh81@gmail.com>
2025-05-04 18:05:48 +0330

    updated color.h to be an alias for vector3

--- src/color.h
+++ src/color.h
@@ -1,44 +1,16 @@
 #ifndef COLOR_H
 #define COLOR_H
 
+#include "vector3.h"
+
 #include <iostream>
-#include <algorithm>
 
-class color {
-public:
-    unsigned __int8 r; //red channel
-    unsigned __int8 g; //green channel
-    unsigned __int8 b; //blue channel
-
-    //constructors
-    color::color(int red, int green, int blue) : r(red), g(green), b(blue) {}
-    color::color(){
-	    r = 0;
-	    g = 0;
-	    b = 0;
-    }
-};
-
-inline color operator+(const color& c1, const color& c2){
-    return color(
-        std::min(255, std::max(0, c1.r + c2.r)),
-        std::min(255, std::max(0, c1.g + c2.g)),
-        std::min(255, std::max(0, c1.b + c2.b))
-    );
-}
-
-inline color operator*(const color& c, double scalar){
-    return color(
-        std::min(255, std::max(0, static_cast<int>(c.r * scalar))),
-        std::min(255, std::max(0, static_cast<int>(c.g * scalar))),
-        std::min(255, std::max(0, static_cast<int>(c.b * scalar)))
-    );
-}
+using color = vector3;
 
 void write_color(std::ostream& out, const color& pixel_color) {
-    auto r = pixel_color.r;
-    auto g = pixel_color.g;
-    auto b = pixel_color.b;
+    auto r = pixel_color.x();
+    auto g = pixel_color.y();
+    auto b = pixel_color.z();
 
     // Translate the [0,1] component values to the byte range [0,255].
     int rbyte = int(255.999 * r);
